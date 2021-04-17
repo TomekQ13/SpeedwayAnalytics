cfc0a348123a776a41c09737c09742334d64046e
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2021-04-17 19:13:35 +0200

    api development finishe

--- interface_api/interface_api/routes.py
+++ interface_api/interface_api/routes.py
@@ -1,4 +1,7 @@
-from flask import Flask, request, jsonify, Blueprint
+from flask import Blueprint, jsonify, request
+from sqlalchemy.exc import IntegrityError
+
+from interface_api import db
 from interface_api.models import Heat, Match
 from interface_api.utils import Serialize
 
@@ -7,39 +10,116 @@ main = Blueprint('main', __name__)
 @main.route('/match', methods = ['GET'])
 def match():
     """Returns a list of all matches or accepts parameters which are columns from the match table and create dynamic filter"""
-    # Checks if there are no unknown query parameters - only match_id is accepted
+    # if there are no parameters then return all matches
+    if not request.args.to_dict():
+        return jsonify(Serialize.serialize_list(Match.query.all(), excl_list=['heats']))
+
+    # Checks if there are no unknown query parameters - if there are display a message
     for key in request.args.keys():
         if key not in Match.__dict__:
             return jsonify({'message': 'Unknown query parameter.'})
 
-    match_id = request.args.get('match_id')
-    print(request.args.to_dict())
-    if match_id:
-        return jsonify(Match.query.get(match_id).serialize(excl_list=['heats']))
-    else:        
-        return jsonify(Serialize.serialize_list(Match.query.filter(request.args.to_dict())))
+    # Takes all GET request parameters and translates them to a SQLAlchemy query parameters
+    query = Match.query
+    for key, value in request.args.items():
+        query = query.filter(getattr(Match, key) == value)    
+    
+    return jsonify(Serialize.serialize_list(query.all(), excl_list=['heats']))
 
 @main.route('/heat', methods = ['GET'])
 def heat():
     """Returns a list of all heats or accepts a query to only return the matching ones"""
-    pass
+    # if there are no parameters then return all matches
+    if not request.args.to_dict():
+        return jsonify(Serialize.serialize_list(Heat.query.all(), excl_list=['match']))
+
+    # Checks if there are no unknown query parameters - if there are display a message
+    for key in request.args.keys():
+        if key not in Heat.__dict__:
+            return jsonify({'message': 'Unknown query parameter.'})
+
+    # Takes all GET request parameters and translates them to a SQLAlchemy query parameters
+    query = Heat.query
+    for key, value in request.args.items():
+        query = query.filter(getattr(Heat, key) == value)    
+
+    return jsonify(Serialize.serialize_list(query.all(), excl_list=['match']))
 
 @main.route('/new_match', methods = ['POST'])
 def new_match():
     """Adds a new match to the match table"""
-    pass
+    attributes = request.get_json()
+    # check if there are any parameters - if no it would cause a runtime error with **
+    if not attributes:
+        return jsonify({'message': 'No parameters provided with the request.'})
+
+    match = Match(**attributes)
+    db.session.add(match)
+
+    # Tries to add the match to the table, if catches IntegrityError it means that integrity constraint was violated
+    try:
+        db.session.commit()
+    except IntegrityError as e:
+        return jsonify({'message': 'Integrity constraint violated', 'error_message': str(e)})
+    except Exception as e:
+        return jsonify({'message': 'Error encountered while trying to add the match', 'error_message': str(e)})
+
+    return jsonify({'message': 'Match added successfully.'})    
 
 @main.route('/new_heat', methods = ['POST'])
 def new_heat():
     """Adds a new heat to the heat table"""
-    pass
+    attributes = request.get_json()
+    # check if there are any parameters - if no it would cause a runtime error with **
+    if not attributes:
+        return jsonify({'message': 'No parameters provided with the request.'})
+ 
+    match = Heat(**attributes)
+    db.session.add(match)
 
-@main.route('/delete_match/<int:match_id>', methods = ['DELETE'])
-def delete_match(match_id):
+    # Tries to add the match to the table, if catches IntegrityError it means that integrity constraint was violated
+    try:
+        db.session.commit()
+    except IntegrityError as e:
+        return jsonify({'message': 'Integrity constraint violated', 'error_message': str(e)})
+    except Exception as e:
+        return jsonify({'message': 'Error encountered while trying to add the match', 'error_message': str(e)})
+
+    return jsonify({'message': 'Match added successfully.'})    
+
+@main.route('/delete_match', methods = ['DELETE'])
+def delete_match():
     """Deletes a match"""
-    pass
+    match_id = request.args.get('match_id')
+    if not match_id:
+        return jsonify({'message': 'Please specify a match_id as a request parameter.'})
 
-@main.route('/delete_heat/<int:heat_id>', methods = ['DELETE'])
+    match = Match.query.get(match_id)
+    if not match:
+        return jsonify({'message': 'Match with this match_id does not exist.'})
+
+    try:
+        db.session.delete(match)
+        db.session.commit()
+        return jsonify({'message': 'Match deleted'})
+    except Exception as e:
+        return jsonify({'message': 'Error occured during delete', 'error_message': str(e)})
+
+
+@main.route('/delete_heat', methods = ['DELETE'])
 def delete_heat():
     """Deletes a heat"""
-    pass
+    heat_id = request.args.get('heat_id')
+    if not heat_id:
+        return jsonify({'message': 'Please specify a heat_id as a request parameter.'})
+
+    heat = Match.query.get(heat_id)
+    if not match:
+        return jsonify({'message': 'Heat with this heat_id does not exist.'})
+
+    try:
+        db.session.delete(heat)
+        db.session.commit()
+        return jsonify({'message': 'Heat deleted'})
+    except Exception as e:
+        return jsonify({'message': 'Error occured during delete', 'error_message': str(e)})
