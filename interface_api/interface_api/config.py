695c9552195a12dc03158637bd4dee433ffa7c5b
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2021-05-10 00:39:06 +0200

    added env variables

--- interface_api/interface_api/config.py
+++ interface_api/interface_api/config.py
@@ -1,6 +1,12 @@
 import os
 
+user = os.environ.get('DATABASE_USER')
+password = os.environ.get('DATABASE_PASSWORD')
+host = os.environ.get('DATABASE_HOST')
+port = os.environ.get('DATABASE_PORT')
+database = os.environ.get('DATABASE_DATABASE')
+
 class Config:
     #SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = DATABASE_CONNECTION_URI = f'postgresql://{user}:{password}@{host}:{port}/{database}'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
