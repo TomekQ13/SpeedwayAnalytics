e5cb5d203202ceeba0fe5d4c60d5aebe69f23f52
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2021-06-10 23:01:35 +0200

    decorator to enforce fitting done

--- analytics/analysis/utils.py
+++ analytics/analysis/utils.py
@@ -1,4 +1,20 @@
 from pandas import DataFrame
+import json
+from functools import wraps
+
+class ObjectNotFittedException(Exception):
+    pass
+
+def require_fit(func):
+    def inner_func(obj, *args):        
+        try:
+            a = obj.col_cats
+        except AttributeError:
+            raise ObjectNotFittedException('Object is not fitted.')
+            
+        func(*args)
+
+    return inner_func
 
 class BinaryEncoder:
     
@@ -6,15 +22,27 @@ class BinaryEncoder:
         self.col_cats = {}
         for column in columns:
             self.col_cats[column] = data[column].unique().tolist()
-        
-        
+
+    @require_fit
+    def transform(self, data:DataFrame):
+
+        pass
+
     def fit_transform(self, data:DataFrame, columns:list):
         self.fit(data, columns)
     
         pass
 
-    def save(self):
-        pass
 
-    def load():
-        pass
+
+    @require_fit
+    def save(self, filename:str):
+        ''' Method to save the encoder. Filename must be txt'''
+        if not self.col_cats:
+            raise ObjectNotFittedException('Object is not fitted.')
+
+        with open(filename, 'w') as f:
+            json.dump(self.col_cats, f)
+
+    def load(self, filename):
+        self.col_cats = json.load(filename)
